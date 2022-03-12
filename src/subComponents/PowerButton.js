f61bd97587853d35efbf3385c590f5bf1f571637
captain-bari <barinder.turkey@gmail.com>
2022-03-13 02:33:15 +0530

    router fix

--- src/subComponents/PowerButton.js
+++ src/subComponents/PowerButton.js
@@ -40,7 +40,7 @@ cursor: pointer;
 const PowerButton = () => {
     return (
         <Power>
-        <NavLink to="/">
+        <NavLink to="/portfolio//">
         <PowerBtn width={30} height={30} fill='currentColor' />
         </NavLink>
         </Power>
