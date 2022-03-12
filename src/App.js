f668c676a568ecb7ba6321c69a33cbc4fbabe69b
captain-bari <barinder.turkey@gmail.com>
2022-03-13 02:28:56 +0530

    router fix

--- src/App.js
+++ src/App.js
@@ -29,11 +29,11 @@ function App() {
 {/* For framer-motion animation on page change! */}
 <AnimatePresence exitBeforeEnter>
 <Switch  location={location} key={location.pathname}>
-      <Route exact path="/" component={Main}/>
-      <Route exact path="/about" component={AboutPage}/>
-      <Route exact path="/blog" component={BlogPage}/>
-      <Route exact path="/work" component={WorkPage}/>
-      <Route exact path="/skills" component={MySkillsPage}/>
+      <Route exact path="/portfolio/" component={Main}/>
+      <Route exact path="/portfolio/about" component={AboutPage}/>
+      <Route exact path="/portfolio/blog" component={BlogPage}/>
+      <Route exact path="/portfolio/work" component={WorkPage}/>
+      <Route exact path="/portfolio/skills" component={MySkillsPage}/>
 
     </Switch>
 </AnimatePresence>
